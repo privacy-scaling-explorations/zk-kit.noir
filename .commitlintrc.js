e9df1bf54d2ac92db047572997279fd74410c45e
sripwoud <me@sripwoud.xyz>
2024-07-02 13:31:38 +0200

    chore: format all files (incl `.nr`) (#5)
    * chore: format with dprint
    
    * chore: format `nr` files with `rustfmt`
    
    * chore: include style check in ci workflow
    
    * chore: rename tests workflow to main
    
    For consistency with other zk-kit repos
    
    * fix: fix yml indentation
    
    * fix: fix output name typo in test workflow
    
    * fix: add id value to step job
    
    * refactor: rename main workflow

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,20 +1,20 @@
-const fs = require("node:fs")
-const path = require("node:path")
+const fs = require('node:fs')
+const path = require('node:path')
 
-const packages = fs.readdirSync(path.resolve(__dirname, "packages"))
+const packages = fs.readdirSync(path.resolve(__dirname, 'packages'))
 
 module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    prompt: {
-        scopes: [...packages],
-        markBreakingChangeMode: true,
-        allowCustomIssuePrefix: false,
-        allowEmptyIssuePrefix: false,
-        issuePrefixes: [
-            {
-                value: "re",
-                name: "re: ISSUES related"
-            }
-        ]
-    }
+  extends: ['@commitlint/config-conventional'],
+  prompt: {
+    scopes: [...packages],
+    markBreakingChangeMode: true,
+    allowCustomIssuePrefix: false,
+    allowEmptyIssuePrefix: false,
+    issuePrefixes: [
+      {
+        value: 're',
+        name: 're: ISSUES related',
+      },
+    ],
+  },
 }
