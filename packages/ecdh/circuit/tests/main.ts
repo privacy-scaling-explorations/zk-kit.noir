3502e2327b00d915f2f9b8228247e1b6a2c4d694
Yash Bharti <yashbharti@Yashs-MacBook-Pro.local>
2024-07-29 12:17:50 +0530

    feat(circuit): ds
    d
    
    re ds

---
+++ packages/ecdh/circuit/tests/main.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
+import { Noir } from '@noir-lang/noir_js';
+import { ProofData } from '@noir-lang/types';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import circuit from '../target/circuit.json';
+import 'mocha';
+// import shelljs from 'shelljs';
+
+// shelljs.exec('npx hardhat compile');
+
+// npx mocha -r ts-node/register tests/main.ts 
+
+describe('It compiles noir program code, receiving circuit bytes and abi object.', () => {
+  let noir: Noir;
+  let correctProof: ProofData;
+
+  beforeEach(async () => {
+    // const circuitFile = readFileSync(resolve(circuit), 'utf-8');
+    // const circuit_file = JSON.parse(circuitFile);
+    const backend = new BarretenbergBackend(circuit);
+    noir = new Noir(circuit, backend);
+  });
+
+  it('Should generate valid proof for correct input', async () => {
+    const input = { x: 1, y: 2 };
+    // Generate proof
+    correctProof = await noir.generateProof(input);
+    expect(correctProof.proof instanceof Uint8Array).to.be.true;
+  });
+
+//   it('Should verify valid proof for correct input', async () => {
+//     const verification = await noir.verifyProof(correctProof);
+//     expect(verification).to.be.true;
+//   });
+
+//   it('Should fail to generate valid proof for incorrect input', async () => {
+//     try {
+//       const input = { x: 1, y: 1 };
+//       const incorrectProof = await noir.generateProof(input);
+//     } catch (err) {
+//       // TODO(Ze): Not sure how detailed we want this it to be
+//       expect(err instanceof Error).to.be.true;
+//       const error = err as Error;
+//       expect(error.message).to.contain('Cannot satisfy constraint');
+//     }
+//   });
+});
--- packages/ecdh/tests/ecdh.tests.ts
+++ packages/ecdh/tests/ecdh.tests.ts
@@ -7,28 +7,27 @@ import { Noir } from '@noir-lang/noir_js';
 import { ProofData } from '@noir-lang/types';
 import { expect } from 'chai';
 import 'mocha';
+import circuit_ecdh from '../../ecdh/target/ecdh.json';
 
-// RUN: npx ts-node tests/ecdh.tests.ts
+// RUN: npx mocha -r ts-node/register tests/ecdh.tests.ts
 // This will not run with yarn tests
 // That is a different setup
 
 function generatePrivateKey(): Uint8Array {
     return randomBytes(32);
 }
-  
+
 describe("ECDH Circuit Tests", function() {
-    let noir: Noir;
-    let correctProof: ProofData;
-    beforeEach(async () => {
-        const circuitFile = readFileSync(resolve('../target/ecdh.json'), 'utf-8');
-        const circuit = JSON.parse(circuitFile);
-        const backend = new BarretenbergBackend(circuit);
-        noir = new Noir(circuit, backend);   
-    });
+    let correctProof;
     it('Should generate valid proof for correct input', async () => {
-       
+        // const circuitFile = readFileSync(resolve('../target/ecdh.json'), 'utf-8');
+        const circuit = JSON.parse(circuit_ecdh);
+        const backend = new BarretenbergBackend(circuit);
+        const noir = new Noir(circuit, backend);
         let pk1 = generatePrivateKey();
         let pk2 = generatePrivateKey();
+
+        // Convert Uint8Array to regular arrays
         const input = {
             private_key1: Array.from(pk1),
             private_key2: Array.from(pk2)
@@ -38,12 +37,36 @@ describe("ECDH Circuit Tests", function() {
         correctProof = await noir.generateProof(input);
         expect(correctProof.proof).to.be.true;
     });
-    
-    it('Should verify valid proof for correct input', async () => {
-        const verification = await noir.verifyProof(correctProof);
-        expect(verification).to.be.true;
-    });
-  
 });
+  
+// describe("ECDH Circuit Tests", function() {
+//     let noir: Noir;
+//     let correctProof: ProofData;
+//     beforeEach(async () => {
+//         const circuitFile = readFileSync(resolve('../target/ecdh.json'), 'utf-8');
+//         const circuit = JSON.parse(circuitFile);
+//         const backend = new BarretenbergBackend(circuit);
+//         noir = new Noir(circuit, backend);   
+//     });
+//     it('Should generate valid proof for correct input', async () => {
+       
+//         let pk1 = generatePrivateKey();
+//         let pk2 = generatePrivateKey();
+//         const input = {
+//             private_key1: Array.from(pk1),
+//             private_key2: Array.from(pk2)
+//         };
+
+//         // Generate proof
+//         correctProof = await noir.generateProof(input);
+//         expect(correctProof.proof).to.be.true;
+//     });
+    
+//     it('Should verify valid proof for correct input', async () => {
+//         const verification = await noir.verifyProof(correctProof);
+//         expect(verification).to.be.true;
+//     });
+  
+// });
 
 
