0d957d9e2eb4f303601c40e4e33a29e18862e923
Yash Bharti <yashbharti@Yashs-MacBook-Pro.local>
2024-07-30 13:41:12 +0530

    feat(ecdh): yO
    YO

--- packages/ecdh/tests/ecdh.tests.ts
+++ packages/ecdh/tests/ecdh.tests.ts
@@ -18,25 +18,36 @@ function generatePrivateKey(): Uint8Array {
 }
 
 describe("ECDH Circuit Tests", function() {
-    let correctProof;
-    it('Should generate valid proof for correct input', async () => {
+    let noir: Noir;
+    let correctProof: ProofData;
+    beforeEach(async () => {
         const circuitFile = readFileSync(resolve(__dirname, '../target/ecdh.json'), 'utf-8');
         const circuit = JSON.parse(circuitFile);
         const backend = new BarretenbergBackend(circuit);
-        const noir = new Noir(circuit, backend);
-        let pk1 = generatePrivateKey();
-        let pk2 = generatePrivateKey();
-
-        // Convert Uint8Array to regular arrays
-        const input = {
-            private_key1: Array.from(pk1),
-            private_key2: Array.from(pk2)
-        };
-
-        // Generate proof
-        correctProof = await noir.generateProof(input);
-        expect(correctProof.proof).to.be.true;
+        noir = new Noir(circuit, backend);
+     
     });
+    it('Should generate valid proof for correct input', async function() {
+        this.timeout(10000); // Set timeout to 10 seconds
+        try {
+            let pk1 = generatePrivateKey();
+            let pk2 = generatePrivateKey();
+            
+            const input = {
+                private_key1: Array.from(pk1),
+                private_key2: Array.from(pk2)
+            };
+    
+            console.log('Generating proof with input:', input);
+            correctProof = await noir.generateProof(input);
+            console.log('Proof generated:', correctProof);
+            console.log("Result: ", correctProof.proof, )
+            expect(correctProof.proof).to.be.true;
+        } catch (error) {
+            console.error('Error generating proof:', error);
+        }
+    });
+    
 });
   
 // describe("ECDH Circuit Tests", function() {
